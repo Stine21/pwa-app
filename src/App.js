9bad518d8d9e067742e9a0e0cde037e10f95b221
Stine21 <72187971+Stine21@users.noreply.github.com>
2022-01-12 11:14:22 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import OneSignal from 'react-onesignal';
+import { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+      OneSignal.init({
+        appId: "69d6e657-f7f9-455e-a2b5-190be465ec7d",
+      });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
